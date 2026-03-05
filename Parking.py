087660bc918513a04c973411c800756435d215f0
Yaroslav Shevchenko <yarben03@gmail.com>
2026-03-05 00:35:06 -0500

    Added Parking ticket class, outputs detail about the car, fine and issuing officer

--- Parking.py
+++ Parking.py
@@ -1,3 +1,5 @@
+import math
+
 class ParkingMeter:
     def __init__(self, minutes_purchased=60):
         self.minutes_purchased = minutes_purchased
@@ -42,6 +44,28 @@ class PoliceOfficer:
         minutes_purchased = meter.minutes_purchased()
 
         if (minutes_parked - minutes_purchased > 0):
-            illigal_minutes = minutes_parked - minutes_purchased
-            return ParkingTicket(car, self, illigal_minutes)
+            illegal_minutes = minutes_parked - minutes_purchased
+            return ParkingTicket(car, self, illegal_minutes)
         else: return None
+
+class ParkingTicket:
+    def __init__(self, car, officer, illegal_minutes):
+        self.car = car
+        self.officer = officer
+        self.illegal_minutes = illegal_minutes
+        self.fine = self.calculate_fine()
+        
+    def calculate_fine(self):
+        hours_over = math.ceil(self.illegal_minutes / 60)
+        
+        fine = 25
+
+        if (hours_over > 1):
+            fine += (hours_over - 1) *10
+        return fine
+    def __str__(self):
+        return (f"   PARKING TICKET   \n"
+                f"Vehicle: {self.car.color} {self.car.make} {self.car.model} (License: {self.car.license_number})\n"
+                f"Minutes Overtime: {self.illegal_minutes}\n"
+                f"Fine Amount: ${self.fine}\n"
+                f"Issued By: Officer {self.officer.name} (Badge: {self.officer.badge_number})\n")
